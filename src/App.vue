9a12efed6f3fe32624b4d0c82b80d864a9ed7ba5
Danil Zaytsev <zaytsevdanil@yandex.ru>
2024-11-30 02:21:43 +0300

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <RouterView />
+  <RouterView/>
+  <ConfirmDialog/>
+  <ConfirmPopup/>
+  <DynamicDialog/>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
+import ConfirmDialog from 'primevue/confirmdialog';
+import ConfirmPopup from 'primevue/confirmpopup';
+import DynamicDialog from 'primevue/dynamicdialog';
+
 </script>
